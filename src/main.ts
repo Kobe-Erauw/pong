d69617e9a2d49efa87e0094223407fe3b0fc3da0
KobeErauw <152495308+KobeErauw@users.noreply.github.com>
2025-01-20 22:47:07 +0100

    added canvas

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const canvas = document.querySelector<HTMLCanvasElement>('canvas');
+if (!canvas) {
+    throw new Error('No canvas element');
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const ctx = canvas.getContext('2d');
+if (!ctx) {
+    throw new Error('No ctx');
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+canvas.width = 800;
+canvas.height = 500;
